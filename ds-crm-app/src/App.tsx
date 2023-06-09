a764786b92f0db3701b8c0030c8e65afe2982fac
Eduardo de Oliveira Duarte <eduardo.duarte@havan.com.br>
2023-06-09 14:38:19 -0300

    Inputs

--- ds-crm-app/src/App.tsx
+++ ds-crm-app/src/App.tsx
@@ -7,6 +7,13 @@ import HomeIcon from '@mui/icons-material/Home';
 import PaymentIcon from '@mui/icons-material/Payment';
 import { Table, TBody, THead, Th, Tr, Td } from "./components/Table";
 import { Card } from "./components/Card";
+import { Form } from "@unform/web";
+import { FormHandles } from '@unform/core';
+import { Input } from "./components/Forms/Input";
+import { useRef } from "react";
+import { SelectInput } from "./components/Forms/Select";
+import { MenuItem } from "@mui/material";
+import { InputCheckBox } from "./components/Forms/CheckBox";
 
 const MenuLinks:Array<ItemMenu> = [
   {
@@ -24,6 +31,8 @@ const MenuLinks:Array<ItemMenu> = [
 ]
 
 function App() {
+  const formRef = useRef<FormHandles>(null);
+
   const Bread:BreadCrumbsLinks = {
     CurrentTitle: "App",
     Pass: [
@@ -33,13 +42,23 @@ function App() {
         }
     ]
 }
+
+const handleData = (data:any) => {
+  console.log(data);
+}
+
   return (
     <Menu MenuItems={MenuLinks}>
       <Page Title="App" BreadCrumb={Bread}>
-        <h3>Página App</h3>
-        <Card $width="50%">
-          This is My Text
-        </Card>
+        <Form onSubmit={handleData} ref={formRef}>
+          <Input name="name" placeholder="Seu Nome..." label="Nome" variant="outlined"/>
+          <SelectInput name="tipo" variant="outlined" label="Tipo">
+            <MenuItem value="admin">Admin</MenuItem>
+            <MenuItem value="commum">Normal</MenuItem>
+          </SelectInput>
+          <InputCheckBox label="lgpd" name="lgpd"/>
+          <LoadButton name="sub" title="Enviar" type="submit"/>
+        </Form>
       </Page>
     </Menu>
   )
